5acb2d722d9a6ba48a3881637c2580f1507741fd
Dylan Lamont <dylanlamont3840@gmail.com>
2021-06-27 20:23:09 +1000

    Implements 5 picks limit validation

--- backend/src/components/card/card.model.js
+++ backend/src/components/card/card.model.js
@@ -12,8 +12,11 @@ const cardSchema = new mongoose.Schema(
   { timestamp: true }
 );
 
-// cardSchema.pre("find", function () {
-//   this.populate(["picks"]);
-// });
+cardSchema.pre("save", function (next) {
+  if (this.picks.length > 5) {
+    return next(new Error("You can't have more than 5 picks"));
+  }
+  next();
+});
 
 export const Card = mongoose.model("Card", cardSchema);
--- backend/src/components/pick/pick.controller.js
+++ backend/src/components/pick/pick.controller.js
@@ -1,10 +1,6 @@
-import { useGenericCRUD } from "../useGenericCRUD";
 import { Card } from "../card/card.model";
-// import { Pick } from "./pick.model";
 import httpErr from "http-errors";
 import asyncHandler from "express-async-handler";
-
-// const generic = useGenericCRUD(Pick);
 
 const addPick = asyncHandler(async (req, res, next) => {
   const createdBy = req.user._id;
@@ -26,6 +22,7 @@ const updatePick = asyncHandler(async (req, res, next) => {
   try {
     const card = await Card.findOne({ createdBy, _id: fromCard });
     card.picks.id(_id).set(req.body);
+    await card.validate();
     await card.save();
     const picksList = card?.picks || [];
     res.status(201).json({ data: picksList });
