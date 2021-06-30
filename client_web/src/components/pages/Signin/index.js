134cbb1cdcbf74f77786bc3b8b93d332de8a9a20
Dylan Lamont <dylanlamont3840@gmail.com>
2021-06-30 20:49:38 +1000

    Implements initial signin/up forms

--- client_web/src/components/pages/Signin/index.js
+++ client_web/src/components/pages/Signin/index.js
@@ -4,55 +4,37 @@ import { Link } from "react-router-dom";
 const Signin = () => {
   return (
     <div>
-      Signin
       <Link to="signup">Sign-up</Link>
-      <form className="container bg-green-100 rounded-lg m-5 p-5">
-        <h1 className="font-black text-4xl p-2 m-3">Sign-up</h1>
+      <form className="container bg-purple-100 rounded-lg m-5 py-10 px-20">
+        <h1 className="font-black text-4xl p-2 m-3">Sign-in</h1>
+
         <div>
-          <label className="text-sm" htmlFor="username">
-            Username
+          <label className="text-sm">
+            Email
+            <br />
+            <input
+              className="rounded-lg text-sm p-1 my-1 border border-purple-400"
+              type="email"
+              required
+            />
           </label>
-          <br />
-          <input
-            className="rounded-lg text-sm p-1 my-1 border border-green-400"
-            type="text"
-            name="username"
-            id="username"
-            required
-          />
         </div>
 
         <div>
-          <label className="text-sm" htmlFor="email">
-            Email{" "}
-          </label>
-          <br />
-          <input
-            className="rounded-lg text-sm p-1 my-1 border border-green-400"
-            type="email"
-            name="email"
-            id="email"
-            required
-          />
-        </div>
-
-        <div>
-          <label className="text-sm" htmlFor="password">
+          <label className="text-sm">
             Password
+            <br />
+            <input
+              className="rounded-lg text-sm p-1 my-1 border border-purple-400"
+              type="password"
+              required
+            />
           </label>
-          <br />
-          <input
-            className="rounded-lg text-sm p-1 my-1 border border-green-400"
-            type="password"
-            name="password"
-            id="password"
-            required
-          />
         </div>
 
         <div>
           <input
-            className="bg-white text-xs rounded-lg p-1 px-3 my-3 border border-purple-700 hover:bg-purple-100"
+            className="bg-white text-xs rounded-lg p-1 px-3 my-3 border border-green-400 hover:bg-green-100"
             type="submit"
             value="Sign-in"
           />
