b0dcdaf9eacd1dc8b72c617c48acd8ede213977b
Dylan Lamont <dylanlamont3840@gmail.com>
2021-07-28 20:25:08 +1000

    Fixes '<li> cannot appear as a descendant of <li>' warning

--- client_web/src/components/PickList/index.js
+++ client_web/src/components/PickList/index.js
@@ -1,14 +1,14 @@
 import Pick from "./Pick";
 
 const PickList = ({ picks }) => (
-  <>
+  <ul>
     {picks &&
       picks.map((pick) => (
         <li key={pick._id}>
           <Pick pick={pick} />
         </li>
       ))}
-  </>
+  </ul>
 );
 
 export default PickList;
