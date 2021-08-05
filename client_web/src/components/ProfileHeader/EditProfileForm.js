065ef16ebc6db9c6bc24c564892967b52f007385
Dylan Lamont <dylanlamont3840@gmail.com>
2021-08-05 22:55:29 +1000

    Fixes profileHeader responsive styling centring

--- client_web/src/components/ProfileHeader/EditProfileForm.js
+++ client_web/src/components/ProfileHeader/EditProfileForm.js
@@ -12,44 +12,46 @@ const EditProfileForm = ({
   const controlledInitialValues = { name, bio, location };
 
   return (
-    <div className="rounded-lg p-8 m-2 border-2 text-xs relative md:m-auto max-w-md border-gray-500">
-      <div className="flex justify-between mb-2">
-        <h5 className="font-bold">Editing Profile</h5>
-        <button onClick={handleSetProfileEditHidden}>Cancel</button>
-      </div>
-      <Formik initialValues={controlledInitialValues} onSubmit={onSubmit}>
-        {({ dirty }) => (
-          <Form>
-            <label>
-              Name:
-              <Field name="name" className="w-full" />
-            </label>
+    <div className="flex justify-center w-full">
+      <div className="relative mx-2 w-full max-w-xl p-8 rounded-lg border-2 border-gray-500">
+        <div className="flex justify-between mb-2">
+          <h5 className="font-bold">Editing Profile</h5>
+          <button onClick={handleSetProfileEditHidden}>Cancel</button>
+        </div>
+        <Formik initialValues={controlledInitialValues} onSubmit={onSubmit}>
+          {({ dirty }) => (
+            <Form>
+              <label>
+                Name:
+                <Field name="name" className="w-full" />
+              </label>
 
-            <label>
-              Bio:
-              <Field name="bio" component="textarea" className="w-full" />
-            </label>
+              <label>
+                Bio:
+                <Field name="bio" component="textarea" className="w-full" />
+              </label>
 
-            <label>
-              Location:
-              <Field name="location" className="w-full" />
-            </label>
-            <div className="flex justify-end mt-2">
-              <button
-                type="submit"
-                disabled={!dirty}
-                className={`border-2 p-2 m-1 rounded-md ${
-                  dirty
-                    ? "border-green-400 text-green-400 hover:bg-green-400 hover:text-white"
-                    : "border-gray-300 text-gray-300 cursor-not-allowed"
-                }`}
-              >
-                {isLoading ? "Updating..." : "Update Profile"}
-              </button>
-            </div>
-          </Form>
-        )}
-      </Formik>
+              <label>
+                Location:
+                <Field name="location" className="w-full" />
+              </label>
+              <div className="flex justify-end mt-2">
+                <button
+                  type="submit"
+                  disabled={!dirty}
+                  className={`border-2 p-2 m-1 rounded-md ${
+                    dirty
+                      ? "border-green-400 text-green-400 hover:bg-green-400 hover:text-white"
+                      : "border-gray-300 text-gray-300 cursor-not-allowed"
+                  }`}
+                >
+                  {isLoading ? "Updating..." : "Update Profile"}
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>{" "}
     </div>
   );
 };
--- client_web/src/components/ProfileHeader/index.js
+++ client_web/src/components/ProfileHeader/index.js
@@ -25,33 +25,35 @@ const ProfileHeader = ({
   if (isLoading) return <small>Loading profile...</small>;
 
   return (
-    <div className="m-2 relative md:m-auto max-w-md p-8 rounded-lg border border-gray-500">
-      {isOwnProfile && (
-        <button
-          className="absolute top-0 right-0 mr-3 md:mr-5 mt-6 text-sm text-gray-500 border border-gray-400 rounded-full p-2 hover:border-blue-500 hover:text-blue-500"
-          onClick={handleSetProfileEditVisible}
-        >
-          Edit Profile
-        </button>
-      )}
+    <div className="flex justify-center w-full">
+      <div className="relative mx-2 w-full max-w-xl p-8 rounded-lg border border-gray-500">
+        {isOwnProfile && (
+          <button
+            className="absolute top-0 right-0 mr-3 md:mr-5 mt-6 text-sm text-gray-500 border border-gray-400 rounded-full p-2 hover:border-blue-500 hover:text-blue-500"
+            onClick={handleSetProfileEditVisible}
+          >
+            Edit Profile
+          </button>
+        )}
 
-      {name ? (
-        <>
-          <h4 className="font-black">{name}</h4>{" "}
-          <p className="text-gray-500">{username}</p>
-        </>
-      ) : (
-        <h4 className="font-black">{username}</h4>
-      )}
-      {bio && <p className="pt-4">{bio}</p>}
-      {location && (
-        <small className="text-gray-500">
-          <span role="img" aria-label="compass" className="mr-2">
-            🧭
-          </span>
-          {location}
-        </small>
-      )}
+        {name ? (
+          <>
+            <h4 className="font-black">{name}</h4>{" "}
+            <p className="text-gray-500">{username}</p>
+          </>
+        ) : (
+          <h4 className="font-black">{username}</h4>
+        )}
+        {bio && <p className="pt-4">{bio}</p>}
+        {location && (
+          <small className="text-gray-500">
+            <span role="img" aria-label="compass" className="mr-2">
+              🧭
+            </span>
+            {location}
+          </small>
+        )}
+      </div>
     </div>
   );
 };
