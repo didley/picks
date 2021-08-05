5d00bd515fd77451fccb02b0ab852baad88370bb
Dylan Lamont <dylanlamont3840@gmail.com>
2021-08-05 21:45:07 +1000

    Implements mock service worker server to enable server mocking with Jest

--- client_web/src/setupTests.js
+++ client_web/src/setupTests.js
@@ -2,4 +2,10 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// sets up mock service worker server to work with jest
+import { server } from "./mocks/server.js";
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
